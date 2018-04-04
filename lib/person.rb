c840b4a14e5cfbc11bb6772e2d6e1c9320452041
AustinPaley <austinpaley32@gmail.com>
2018-04-04 08:45:07 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(name)
+    @name = name
+  end
+    #binding.pry
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+end
